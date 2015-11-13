184823deb9204dd036a8a35da42ca6293d20b143
GaryHendrick <gary.hendrick@gmail.com>
2015-11-13 10:34:44 -0500

    Committed naming convention changes

---
+++ com/garyhendrick/matplotstylist/__init__.py
@@ -0,0 +1,10 @@
+__author__ = 'gary'
+""" Note repaired matplotstylist.py so that its launcher methods do not leave an abandoned thread upon exit
+    This was accomplished by having the application subclass Frame and by not creating a root explicitly,
+    in direct opposition to most applications"""
+#TODO replace each button with icons
+#TODO change naming conventions to better describe the use case for this project
+#TODO add a readme to the github repo
+#TODO add license information to the source
+#TODO configurre __author__ etc... to be presentable
+#TODO connect github issue tracker
